f6ffecf7eac987389080fc1edaf4c2de142b5b24
jdpgrailsdev <jpearlin1@gmail.com>
2019-01-15 14:56:08 -0500

    Rough cut of timeline app

---
+++ src/Timeline.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { Timeline, TimelineEvent } from "react-event-timeline";
+
+export default () => {
+  return (
+    <Timeline>
+      <TimelineEvent
+        title="Mark Coyle recording session, Mark Coyle Home Studio, Manchester, UK"
+        createdAt="Late 1992/Early 1993"
+        icon={<i className="material-icons md-18">settings_voice</i>}
+      >
+        Early recording session at Mark Coyle's home studio on Mauldeth Road West in Manchester, UK over
+        various dates from late 1992 to early 1993.  Songs recorded in these sessions include "D'Yer Wanna Be a Spaceman?", "Going Nowhere", "Hello", "Married With Children", "Rockin' Chair" and "She's Electric"
+      </TimelineEvent>
+      <TimelineEvent
+        title="Real People Recording Session, Liverpool, UK"
+//        onClick={() => alert("clicked")}
+        createdAt="Spring 1993"
+        icon={<i className="material-icons md-18">settings_voice</i>}
+      >
+        Recording session at the Porter Street Studio in Liverpool, UK that produced the "<a href="https://en.wikipedia.org/wiki/Live_Demonstration" target="_blank">Live Demonstration"</a> tape.
+      </TimelineEvent>
+    </Timeline>
+  );
+};
