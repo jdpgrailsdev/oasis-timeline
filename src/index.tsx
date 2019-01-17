cf6ed8612a646e700a7aabe7ff32585ba543a153
Jonathan Pearlin <jpearlin@newrelic.com>
2019-01-17 11:57:22 -0500

    Update site content
    Add filtering
    Cleanup style
    Add more timeline events
    Add missing icons

--- src/index.tsx
+++ src/index.tsx
@@ -22,40 +22,63 @@ class App extends React.Component<any, any> {
   handleInputChange(event: any) {
     const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
+    const name = target.name.toLowerCase();
 
     this.setState({
       [name]: value
     });
+
+    this.filterTimeline(name, value);
+  }
+
+  filterTimeline(name: any, value: any) {
+    const node = ReactDOM.findDOMNode(this);
+    if (node instanceof HTMLElement) {
+        Array.from(node.querySelectorAll('.' + name)).forEach(
+            (element, index, array) => {
+                if(element instanceof HTMLElement) {
+                    if(value == true) {
+                        element.style.display = 'block';
+                    } else {
+                        element.style.display = 'none';
+                    }
+                }
+            }
+        );
+    }
+  }
+
+  showSettings(event: any) {
+    event.preventDefault();
   }
 
   render() {
       return(
         <div>
-        <h1>Oasis Timeline</h1>
-        <div className="filter">Filter</div>
-        <form>
-          <label>
-              Recordings
-              <input name="Recordings" type="checkbox" checked={this.state.recordings} onChange={this.handleInputChange}/>
-          </label>
-          <label>
-              Releases
-              <input name="Releases" type="checkbox" checked={this.state.releases} onChange={this.handleInputChange}/>
-          </label>
-          <label>
-              Gigs
-              <input name="Gigs" type="checkbox" checked={this.state.gigs} onChange={this.handleInputChange}/>
-          </label>
-          <label>
-              Noteworthy Events
-              <input name="Noteworthy" type="checkbox" checked={this.state.noteworthy} onChange={this.handleInputChange}/>
-          </label>
-        </form>
-        <br />
-        <div>
-          <Timeline />
-        </div>
+            <h1><img src="images/oasis-logo.jpg" alt="oasis" className="logo" /> Timeline</h1>
+            <div className="filter">Filter</div>
+            <form>
+              <label>
+                  Recordings
+                  <input name="Recordings" type="checkbox" defaultChecked={this.state.recordings} onChange={this.handleInputChange}/>
+              </label>
+              <label>
+                  Releases
+                  <input name="Releases" type="checkbox" defaultChecked={this.state.releases} onChange={this.handleInputChange}/>
+              </label>
+              <label>
+                  Important Gigs
+                  <input name="Gigs" type="checkbox" defaultChecked={this.state.gigs} onChange={this.handleInputChange}/>
+              </label>
+              <label>
+                  Noteworthy Events
+                  <input name="Noteworthy" type="checkbox" defaultChecked={this.state.noteworthy} onChange={this.handleInputChange}/>
+              </label>
+            </form>
+            <br />
+            <div>
+              <Timeline />
+            </div>
       </div>
     );
   }
