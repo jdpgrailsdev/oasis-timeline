eab9e2d6d7e2117a95da166e6c0a18fcb11c839d
Jonathan Pearlin <jpearlin@newrelic.com>
2019-01-18 20:22:33 -0500

    Fix local links

--- src/Home.tsx
+++ src/Home.tsx
@@ -8,7 +8,7 @@ class Home extends React.Component<any, any> {
                 <h2>Welcome</h2>
                 <div className="mainText">
                 This site strives to be the canonical, historical timeline for the British rock band <a href="https://www.oasisinet.com" target="_blank">Oasis</a>.  The goal is to document key events in the career of Oasis, such as recording sessions,
-                important gigs, releases and other noteworthy items.  The project is a work in progress and will continue to be updated as new information comes to light.  The <a href="/timeline#/timeline">timeline</a> maintained by this site has been compiled
+                important gigs, releases and other noteworthy items.  The project is a work in progress and will continue to be updated as new information comes to light.  The <a href="/#/timeline">timeline</a> maintained by this site has been compiled
                 from various sources to date, including (but not limited to):
                 <ul>
                     <li>Officially released material</li>
@@ -17,7 +17,7 @@ class Home extends React.Component<any, any> {
                     <li>Fan sites</li>
                     <li>Message boards</li>
                 </ul>
-                Please head over to the <a href="/timeline#/contributing">how to contribute page</a> to see how you can help make this the most complete timeline for Oasis!
+                Please head over to the <a href="/#/contributing">how to contribute page</a> to see how you can help make this the most complete timeline for Oasis!
                 <p>
                 <b>N.B.</b> Oasis Timeline is an unofficial fan website and is not related to Oasis, Sony Music, or Ignition Management.
                 </p>
