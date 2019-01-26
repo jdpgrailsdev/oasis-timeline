a7af8f5f36c4517c650a754fe5546239441f9929
jdpgrailsdev <jpearlin1@gmail.com>
2019-01-26 16:51:54 -0500

    Updating site.

---
+++ precache-manifest.bd3d16cb2e1585335d76898f7f8312e4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3dea9049d2ffe2801dfb",
+    "url": "/oasis-timeline/static/js/runtime~main.3dea9049.js"
+  },
+  {
+    "revision": "fbb7a66f41cda1c7f9c6",
+    "url": "/oasis-timeline/static/js/main.fbb7a66f.chunk.js"
+  },
+  {
+    "revision": "18588e0ed8c0df2bff83",
+    "url": "/oasis-timeline/static/js/1.18588e0e.chunk.js"
+  },
+  {
+    "revision": "f443829189623cd4d2ad4a12ed16e3c3",
+    "url": "/oasis-timeline/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/oasis-timeline/precache-manifest.1febacd6373ad46d6ff1243162629591.js"
+  "/oasis-timeline/precache-manifest.bd3d16cb2e1585335d76898f7f8312e4.js"
 );
 
 workbox.clientsClaim();
