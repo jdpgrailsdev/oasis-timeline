e1dac728a3234ee7f6a884947bdae523a107a15a
jdpgrailsdev <jpearlin1@gmail.com>
2019-02-15 10:52:51 -0500

    Updating site.

--- precache-manifest.8019ba2d51811421c708d89fa9034cab.js
+++ precache-manifest.8019ba2d51811421c708d89fa9034cab.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "04d2796375b302d59b72",
-    "url": "/static/js/main.04d27963.chunk.js"
+    "revision": "b3a349e04defee5dee39",
+    "url": "/static/js/main.b3a349e0.chunk.js"
   },
   {
     "revision": "c0781b82b34290c5c6cf",
     "url": "/static/js/1.c0781b82.chunk.js"
   },
   {
-    "revision": "a8852fd610f91486479195a47ecad3a5",
+    "revision": "cccd9a519bef7aa2bb877304336973af",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.863e0a49a5762f612570ded1d86e6d6a.js"
+  "/precache-manifest.8019ba2d51811421c708d89fa9034cab.js"
 );
 
 workbox.clientsClaim();
