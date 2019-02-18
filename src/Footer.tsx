fe94194539b65ad069cec36a2a42112eca49ac64
Jonathan Pearlin <jpearlin@newrelic.com>
2019-02-18 14:44:11 -0500

    Make site responsive to mobile devices

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import MediaQuery from "react-responsive";
 
 class Footer extends React.Component<any, any> {
 
@@ -11,8 +12,15 @@ class Footer extends React.Component<any, any> {
                 <br />
                 <br />
                 <footer>
-                    <span>&copy; 2019</span>
-                    <span>Last Updated: {process.env.REACT_APP_UPDATED_AT}</span>
+                    <MediaQuery query="(min-device-width: 768px)">
+                        <span>&copy; 2019</span>
+                        <span>Last Updated: {process.env.REACT_APP_UPDATED_AT}</span>
+                    </MediaQuery>
+                    <MediaQuery query="(max-device-width: 767px)">
+                        <span>&copy; 2019</span>
+                        <span>Last Updated</span>
+                        <span>{process.env.REACT_APP_UPDATED_AT}</span>
+                    </MediaQuery>
                 </footer>
             </div>
         );
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { HashLink as Link } from "react-router-hash-link";
 import Footer from "./Footer";
 import TimelineData from './data/timelineData.js';
 import TodayInHistory from "./TodayInHistory";
@@ -7,7 +8,7 @@ class Home extends React.Component<any, any> {
 
     render() {
         return(
-            <div className="main">
+            <div className="main" id="top">
                 <h2>Welcome</h2>
                 <div className="mainText">
                     This site strives to be the most accurate timeline of events in the career of the British rock band <a href="http://www.oasisinet.com"  target="_blank" rel="noopener noreferrer">Oasis</a>.  To date, the timeline contains <b>{TimelineData.getNumberOfEvents()}</b> events covering <b>{TimelineData.getNumberOfYears()}</b> years.  If you have an event that you feel is important or have spotted a discrepancy, please head over to the <a href="#/contributing#disqus_thread">how to contribute page</a> to see how you can help make this the most complete timeline for Oasis!
@@ -16,17 +17,17 @@ class Home extends React.Component<any, any> {
                 <h3>What Is Included</h3>
                 <div className="mainText">
                     The timeline documents key envets, such as recording sessions, important gigs, releases and other noteworthy items.  The project is a work in progress and will continue to be updated as new information comes to light.
-                    <p>
-                        The <a href="#/timeline">timeline</a> maintained by this site has been compiled from various sources to date, including (but not limited to):
-                        <ul>
-                            <li>Officially released material</li>
-                            <li>Contemporaneous news articles</li>
-                            <li>Interviews</li>
-                            <li>Biographies and other books about the band</li>
-                            <li>Fan sites</li>
-                            <li>Message boards</li>
-                        </ul>
-                    </p>
+                    <br />
+                    <br />
+                    The <a href="#/timeline">timeline</a> maintained by this site has been compiled from various sources to date, including (but not limited to):
+                    <ul>
+                        <li>Officially released material</li>
+                        <li>Contemporaneous news articles</li>
+                        <li>Interviews</li>
+                        <li>Biographies and other books about the band</li>
+                        <li>Fan sites</li>
+                        <li>Message boards</li>
+                    </ul>
                 </div>
                 <h3>What is <b>NOT</b> Included</h3>
                 <div className="mainText">
@@ -37,6 +38,11 @@ class Home extends React.Component<any, any> {
                     <b>N.B.</b> Oasis Timeline is an unofficial fan website and is not related to Oasis, Epic Records, Sony Music, or Ignition Management.  All information contained within the site is soley for entertainment purposes only and is used within the scope of "fair use" purposes.
                     <p>Please see the <a href="#/sources">sources page</a> for a complete list of source material used to confirm the dates listed on this site.  The Oasis Timeline is not responsible for the content of source material and does not necessarily endorse the views expressed within.</p>
                 </div>
+                <br />
+                <br />
+                <span className="backToTop">
+                    <Link to="/home#top">Back To Top</Link>
+                </span>
                 <Footer />
             </div>
         );
