0249029e96f83657155cbd84a5c75d1918cefce0
jdpgrailsdev <jpearlin1@gmail.com>
2019-12-31 14:51:18 -0500

    Updating site.

---
+++ precache-manifest.87ddf2bd37520c2e84d79dfb3de0d65f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "61e1aed15dc009c15ce0",
+    "url": "/static/js/main.61e1aed1.chunk.js"
+  },
+  {
+    "revision": "f2973f632e2402aafd7d",
+    "url": "/static/js/1.f2973f63.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eb59b9a7a9e49c5c5b77c3b78e2796d6",
+    "url": "/index.html"
+  }
+];
