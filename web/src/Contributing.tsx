0c47b132145949082736faac34105beb5c3cb91d
jdpgrailsdev <jpearlin1@gmail.com>
2020-04-27 08:58:29 -0400

    Add tweet link

--- web/src/Contributing.tsx
+++ web/src/Contributing.tsx
@@ -28,7 +28,7 @@ export default class Contributing extends React.Component<any, any> {
                 <div className="mainText">
                     The goal of this site is to be as accurate as possible.  If you have information that may improve the timeline or have an event that you believe should be included in
                     the timeline, please do not hesitate to contact us!  The best place to do so is to add a comment to the <a href="http://live4ever.proboards.com/thread/90673/oasis-timeline-project" target="_blank" rel="noopener noreferrer">Oasis Timeline thread</a> on the <a href="http://live4ever.proboards.com/" target="_blank" rel="noopener noreferrer">Live4ever Forum</a>.
-                    You may also make a comment <Link to="/contributing#disqus_thread">below</Link> via <a href="https://disqus.com" target="_blank" rel="noopener noreferrer">Disqus</a>.
+                    You may also make a comment <Link to="/contributing#disqus_thread">below</Link> via <a href="https://disqus.com" target="_blank" rel="noopener noreferrer">Disqus</a> or <a href="https://twitter.com/intent/tweet?screen_name=OasisTimeline&ref_src=twsrc%5Etfw" className="twitter-mention-button" data-show-count="false" target="_blank" rel="noopener noreferrer">Tweet to @OasisTimeline</a><script async src="https://platform.twitter.com/widgets.js"></script>.
                     <br />
                     <br />
                     Likewise, if you find an issue with the site itself (bug, typo or other problem), please create an issue over at the <a href="https://github.com/jdpgrailsdev/oasis-timeline/issues" target="_blank" rel="noopener noreferrer">site's repository in GitHub.</a>
